ea23f335006ed1451bb7d64d6872dcab9eb02d99
yanitraabdi <streetways@gmail.com>
2017-09-11 03:27:18 +0700

    Login

--- Merachel/Controllers/AdminUserController.cs
+++ Merachel/Controllers/AdminUserController.cs
@@ -20,5 +20,37 @@ namespace Merachel.Controllers
         {
             return CustomView("Users");
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(UserProfile objUser)
+        {
+            if (ModelState.IsValid)
+            {
+                using (DB_Entities db = new DB_Entities())
+                {
+                    var obj = db.UserProfiles.Where(a => a.UserName.Equals(objUser.UserName) && a.Password.Equals(objUser.Password)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        Session["UserID"] = obj.UserId.ToString();
+                        Session["UserName"] = obj.UserName.ToString();
+                        return RedirectToAction("UserDashBoard");
+                    }
+                }
+            }
+            return View(objUser);
+        }
+
+        public ActionResult UserDashBoard()
+        {
+            if (Session["UserID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
     }
 }
